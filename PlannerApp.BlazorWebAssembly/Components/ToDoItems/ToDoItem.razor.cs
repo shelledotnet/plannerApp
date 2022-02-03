fe88d0f953221f5f9bccc7a65f53587e5d2b7388
shelle <mohammed.shelle@fcmb.com>
2022-02-03 05:07:21 +0100

    update on planner App v2

---
+++ PlannerApp.BlazorWebAssembly/Components/ToDoItems/ToDoItem.razor.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using PlannerApp.Shared.Models;
+
+namespace PlannerApp.BlazorWebAssembly.Components.ToDoItems
+{
+    public partial class ToDoItem
+    {
+        [Parameter]
+        public ToDoItemDetail Item { get; set; }
+
+
+    }
+}
--- PlannerApp.BlazorWebAssembly/Program.cs
+++ PlannerApp.BlazorWebAssembly/Program.cs
@@ -1,50 +1,37 @@
 using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using MudBlazor.Services;
+using PlannerApp.BlazorWebAssembly;
 using PlannerApp.Services;
-using PlannerApp.Services.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace PlannerApp.BlazorWebAssembly
-{
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
 
-            //the system registered httpclient that has base address below is actually pointing to the project address which in our case we are consuming api from a vendor so we dont need it
-            // builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            
-            
-            //using Microsoft.Extensions.http
-            builder.Services.AddHttpClient("PlannerApp.Api",client=> 
-            {
-                client.BaseAddress = new Uri("https://plannerapp-api.azurewebsites.net/" );
-            
-            }).AddHttpMessageHandler<AuthorizationMessagehandler>();
-            //the esence of the message handler above is to force the request(create ,read,update,delete)andresponse to go through the message handler function sendAsync() in other to set access token on the header of every request
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
 
-            //this register httpclient service whose name is PlannerApp.Api
-            builder.Services.AddScoped(sp => sp.GetService<IHttpClientFactory>().CreateClient("PlannerApp.Api"));
+//this is how to refere to the header tag after means anything within header tag like title tag in html pages
+builder.RootComponents.Add<HeadOutlet>("head::after");//HeadOutlet enable <PageTitle> in the pages
 
-            builder.Services.AddTransient<AuthorizationMessagehandler>();
-            builder.Services.AddBlazoredLocalStorage();
-            builder.Services.AddAuthorizationCore();//this will allow us to use Authorize attribute
-            builder.Services.AddScoped<AuthenticationStateProvider, JwtAuthenticationStateProvider>();
-            builder.Services.AddHttpClientService(); 
-            builder.Services.AddMudServices();
-            await builder.Build().RunAsync();
-        }
-    }
-}
+//the system registered httpclient that has base address below is actually pointing to the project address which in our case we are consuming api from a vendor so we dont need it
+// builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+
+//using Microsoft.Extensions.http
+builder.Services.AddHttpClient("PlannerApp.Api", client =>
+ {
+     client.BaseAddress = new Uri("https://plannerapp-api.azurewebsites.net/");
+
+ }).AddHttpMessageHandler<AuthorizationMessagehandler>();
+//the esence of the message handler above is to force the request(create ,read,update,delete)andresponse to go through the message handler function sendAsync() in other to set access token on the header of every request
+
+//this register httpclient service whose name is PlannerApp.Api
+builder.Services.AddScoped(sp => sp.GetService<IHttpClientFactory>().CreateClient("PlannerApp.Api"));
+
+builder.Services.AddTransient<AuthorizationMessagehandler>();
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddAuthorizationCore();//this will allow us to use Authorize attribute
+builder.Services.AddScoped<AuthenticationStateProvider, JwtAuthenticationStateProvider>();
+builder.Services.AddHttpClientService();
+builder.Services.AddMudServices();
+await builder.Build().RunAsync();
